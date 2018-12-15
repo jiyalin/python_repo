15df78f456e6616c4a4811381cc7693f510dd34f
yalin <jiyalin@gmail.com>
2018-12-15 22:49:36 +0800

    123

--- Learn/demo1.py
+++ Learn/demo1.py
@@ -1,1 +1,6 @@
+import wget
 
+
+for i in range(10):
+    wget.download("http://0d61224e92fa41a1887cb35a759aa4e1-cn-shanghai.alicloudapi.com/test1?dt=20181204")
+    print( "round" & i & " is done")
